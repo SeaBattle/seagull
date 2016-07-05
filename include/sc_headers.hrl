e97fcd5f079d48ad3e9295e5a6f4fc5dd4440ff1
Tihon <com.tihon@mail.ru>
2016-07-05 19:00:24 +0300

    fix names and add CONF suffix

--- include/sc_headers.hrl
+++ include/sc_headers.hrl
@@ -8,25 +8,25 @@
 -author("tihon").
 
 %% Database conf vars
--define(DATABASE_HOSTS, <<"database/hosts">>).
--define(DATABASE_DB, <<"database/db">>).
--define(DATABASE_LOGIN, <<"database/login">>).
--define(DATABASE_PASS, <<"database/pass">>).
--define(DATABASE_SIZE, <<"database/pool/size">>).
--define(DATABASE_OVERFLOW, <<"database/pool/overflow">>).
--define(DATABASE_OVERFLOW_TTL, <<"database/pool/overflow_ttl">>).
--define(DATABASE_OVERFLOW_CHECK, <<"database/pool/overflow_check">>).
+-define(DATABASE_HOSTS_CONF, <<"database/hosts">>).
+-define(DATABASE_DB_CONF, <<"database/db">>).
+-define(DATABASE_LOGIN_CONF, <<"database/login">>).
+-define(DATABASE_PASS_CONF, <<"database/pass">>).
+-define(DATABASE_SIZE_CONF, <<"database/pool/size">>).
+-define(DATABASE_OVERFLOW_CONF, <<"database/pool/overflow">>).
+-define(DATABASE_OVERFLOW_TTL_CONF, <<"database/pool/overflow_ttl">>).
+-define(DATABASE_OVERFLOW_CHECK_CONF, <<"database/pool/overflow_check">>).
 
 %% Email conf vars
--define(MAILGUN_DOMAIN, <<"email/domain">>).
--define(MAILGUN_API_URL, <<"email/api_url">>).
--define(MAILGUN_API_KEY, <<"email/api_key">>).
+-define(MAILGUN_DOMAIN_CONF, <<"email/domain">>).
+-define(MAILGUN_API_URL_CONF, <<"email/api_url">>).
+-define(MAILGUN_API_KEY_CONF, <<"email/api_key">>).
 
 %% Auth conf vars
--define(MAILGUN_DOMAIN, <<"auth/salt_len">>).
--define(MAILGUN_API_URL, <<"auth/secret_len">>).
--define(MAILGUN_API_KEY, <<"auth/secret_iter">>).
+-define(SALT_LEN_CONF, <<"auth/salt_len">>).
+-define(SECRET_LEN_CONF, <<"auth/secret_len">>).
+-define(SECRET_ITER_CONF, <<"auth/secret_iter">>).
 
 %% UserService conf
--define(USER_SERVICE_HTTP_PORT, <<"user_service/http/port">>).
--define(USER_SERVICE_HTTP_ACCEPTORS, <<"user_service/http/acceptors">>).
+-define(USER_SERVICE_HTTP_PORT_CONF, <<"user_service/http/port">>).
+-define(USER_SERVICE_HTTP_ACCEPTORS_CONF, <<"user_service/http/acceptors">>).
